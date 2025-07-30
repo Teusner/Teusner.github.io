e820e588da725c4481dedf40dd84c5f4bd241e24
Teusner <47608094+Teusner@users.noreply.github.com>
2025-07-30 14:15:34 +0000

    Deploying to gh-pages from @ Teusner/Teusner.github.io@98eceb4c102bc262cc4b12bd75a4fb4d30efa937 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -80,66 +80,16 @@ ninja.data = [{
             window.location.href = "/blog/2024/my-logo/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{id: "projects-stable-cycles-icra2025",
+      },{id: "projects-stable-cycles-icra2025",
           title: 'Stable Cycles ICRA2025',
           description: "ICRA2025 contribution about stable cycles in robotics",
           section: "Projects",handler: () => {
               window.location.href = "/projects/icra2025/";
-            },},{id: "projects-quentin-brateau-phd-thesis",
-          title: 'Quentin Brateau PhD Thesis',
-          description: "ENSTA Brest PhD thesis on event-driven cycle navigation for mobile robots.",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/thesis/";
+            },},{id: "teaching-subsurface",
+          title: 'Subsurface',
+          description: "Courses about the fabrication of a Low Cost Subsurface AUV",
+          section: "Teaching",handler: () => {
+              window.location.href = "/teaching/subsurface/";
             },},{
         id: 'social-github',
         title: 'GitHub',
